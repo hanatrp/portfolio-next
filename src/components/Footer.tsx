408c20c80bf9d3c527fe15d1941887fe46f94cbe
Hana Nur Aisyah <hananurtryn@gmail.com>
2024-10-25 14:58:16 +0700

    ui

---
+++ src/components/Footer.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { RxGithubLogo, RxInstagramLogo, RxLinkedinLogo } from "react-icons/rx";
+
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] z-[22] ">
+      <div className="w-full flex flex-col items-center justify-center m-auto">
+        <div className="min-w-[200px] h-auto flex flex-row items-center justify-between">
+          <Link
+            href="https://www.instagram.com/hanatrp/"
+            className="cursor-pointer m-9"
+          >
+            <RxInstagramLogo />
+          </Link>
+          <Link
+            href="https://www.github.com/hanatrp/"
+            className=" cursor-pointer m-9"
+          >
+            <RxGithubLogo />
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/hana-nur-aisyah-8591b8222/"
+            className=" cursor-pointer m-9"
+          >
+            <RxLinkedinLogo />
+          </Link>
+        </div>
+
+        <div className="mb-[20px] text-[15px] text-center">
+          &copy; Hana Nur Aisyah 2024 Inc. All rights reserved
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Project.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ProjectCard from "./sub/ProjectCard";
+
+const Projects = () => {
+  return (
+    <div
+      className="flex flex-col items-center justify-center py-20 z-[22]"
+      id="projects"
+    >
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+        My Projects
+      </h1>
+      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+        <ProjectCard
+          src="/bangking.png"
+          title="Horizon - Banking App"
+          description="A banking app for access and manage account and transactions. Built with Next.js, TypeScript, Tailwind CSS, Prisma ORM. "
+          link="https://banking-amber.vercel.app/"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
